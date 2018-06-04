c54bd43e4b57a008a499223e0abc7215192f93cd
zarcao <zhouhao@aigou.com>
2018-06-04 14:59:32 +0800

    blog App基本完成

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(default='标题',max_length=50)
+    date = models.DateField()
+    image = models.ImageField(default='default.png',upload_to='images/')
+    text = models.TextField(default='啥都没写啊')
+
+    def __str__(self):
+        return self.title
