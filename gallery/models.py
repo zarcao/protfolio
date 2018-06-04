aae7d628aecc07740bb1cf988c571ca5b60b515e
zarcao <zhouhao@aigou.com>
2018-06-04 10:45:40 +0800

    添加上传图片功能

--- gallery/models.py
+++ gallery/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(default='这小子什么都没留下', max_length=100)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='作品标题', max_length=50)
 
+    def __str__(self):
+        return self.title
