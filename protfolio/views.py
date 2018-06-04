6246efbacac9b1c39f93d5292001051e31077c1e
zarcao <zhouhao@aigou.com>
2018-06-04 10:01:31 +0800

    建立model

--- protfolio/views.py
+++ protfolio/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from gallery.models import Gallery
 
 def home(request):
-    return render(request,'home.html')
+    gallerys = Gallery.objects
+    return render(request,'home.html',{'gallerys':gallerys})
