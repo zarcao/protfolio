7abbdc6798bbc913267c67d8ef81591d9b1d8332
zarcao <zhouhao@aigou.com>
2018-06-05 10:47:35 +0800

    详细页面完成

--- protfolio/views.py
+++ protfolio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from gallery.models import Gallery
 
 def home(request):
@@ -6,6 +6,5 @@ def home(request):
     return render(request,'home.html',{'gallerys':gallerys})
 
 def detail(request, gallery_id):
-    gallerys = Gallery.objects.filter(id = gallery_id)
-    print(gallerys)
+    gallerys = get_object_or_404(Gallery, pk=gallery_id,)
     return render(request, 'detail.html', {'gallerys': gallerys})
