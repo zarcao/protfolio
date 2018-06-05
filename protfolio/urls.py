743233a6b27b62519968814760ba63ca45d8a347
zarcao <zhouhao@aigou.com>
2018-06-05 10:12:40 +0800

    添加了详细detail页面，并且把a标签的下横线去掉

--- protfolio/urls.py
+++ protfolio/urls.py
@@ -24,5 +24,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('<int:gallery_id>/', views.detail),
     path('blog/', include('blog.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- protfolio/views.py
+++ protfolio/views.py
@@ -4,3 +4,8 @@ from gallery.models import Gallery
 def home(request):
     gallerys = Gallery.objects
     return render(request,'home.html',{'gallerys':gallerys})
+
+def detail(request, gallery_id):
+    gallerys = Gallery.objects.filter(id = gallery_id)
+    print(gallerys)
+    return render(request, 'detail.html', {'gallerys': gallerys})
